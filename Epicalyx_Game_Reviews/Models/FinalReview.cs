2c79d3e5b538993b82988057a37b5ef4b07b28df
George Jamieson <ac111480@avcol.school.nz>
2022-03-18 10:10:57 +1300

    creating tables

--- Epicalyx_Game_Reviews/Models/FinalReview.cs
+++ Epicalyx_Game_Reviews/Models/FinalReview.cs
@@ -2,5 +2,15 @@
 {
     public class FinalReview
     {
+       public int FinalReviewID { get; set; }
+       public string Review { get; set; }
+       public int FinalRating { get; set; }
+       public int Genre { get; set; }
+       public int Publisher { get; set; }
+
+
+       public Game Game { get; set; }
+       public User User { get; set; }
+        
     }
 }
--- Epicalyx_Game_Reviews/Models/User.cs
+++ Epicalyx_Game_Reviews/Models/User.cs
@@ -1,6 +1,16 @@
-﻿namespace Epicalyx_Game_Reviews.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace Epicalyx_Game_Reviews.Models
 {
     public class User
     {
+        public int UserID { get; set; }
+        public string Username { get; set; }
+        public int Age { get; set; }
+        public int ProfilePic { get; set; }
+
+        public ICollection<FinalReview> FinalReview { get; set; }
     }
 }
